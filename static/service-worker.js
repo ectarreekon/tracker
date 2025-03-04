2b38e9aca41cbe23a57d02fc1ebc310192c42fcb
ectarreekon <ectarreekon@gmail.com>
2025-03-05 05:39:15 +0800

    Create service-worker.js

---
+++ static/service-worker.js
@@ -0,0 +1,27 @@
+const CACHE_NAME = 'location-tracker-v1';
+const urlsToCache = [
+    '/',
+    '/static/styles.css',
+    '/manifest.json'
+];
+
+// Install event
+self.addEventListener('install', (event) => {
+    event.waitUntil(
+        caches.open(CACHE_NAME)
+            .then((cache) => cache.addAll(urlsToCache))
+    );
+});
+
+// Fetch event
+self.addEventListener('fetch', (event) => {
+    event.respondWith(
+        caches.match(event.request)
+            .then((response) => {
+                if (response) {
+                    return response;
+                }
+                return fetch(event.request);
+            })
+    );
+});
